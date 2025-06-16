c83c66a8935812069707b15369c2696c0b8402c0
aboudev22 <aboubakarjay2@gmail.com>
2025-06-16 20:01:12 +0100

    fix gap bug

--- src/components/NotificationsIOS.tsx
+++ src/components/NotificationsIOS.tsx
@@ -6,32 +6,34 @@ export default function NotificationsIOS() {
   const [isCollapse, setIsCollapse] = useState(false);
   const toggleCollapse = () => setIsCollapse(!isCollapse);
 
-  const cards = [0, 1, 2, 4, 5];
+  const cards = [0, 1, 2];
 
   return (
     <div className="w-sm h-96 bg-neutral-900 flex gap-2 flex-col px-4 pt-20 justify-start items-center relative overflow-hidden">
-      <AnimatePresence>
-        {!isCollapse && (
-          <motion.div
-            transition={{ duration: 0.3, ease: "anticipate" }}
-            initial={{ y: 20, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            exit={{ y: 20, opacity: 0 }}
-            className="w-52 flex justify-between z-0"
-          >
-            <p className="text-white text-xs font-bold">Notifications</p>
-            <button
-              onClick={toggleCollapse}
-              className="px-1 py-[1px] text-xs text-white hover:text-black bg-neutral-800 hover:bg-white rounded-full cursor-pointer transition-all duration-300"
+      <div className="h-6 w-52 flex justify-between z-0 relative">
+        <AnimatePresence>
+          {!isCollapse && (
+            <motion.div
+              transition={{ duration: 0.3, ease: "anticipate" }}
+              initial={{ y: 20, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: 20, opacity: 0 }}
+              className="absolute top-0 left-0 w-full flex justify-between"
             >
-              collapse
-            </button>
-          </motion.div>
-        )}
-      </AnimatePresence>
+              <p className="text-white text-xs font-bold">Notifications</p>
+              <button
+                onClick={toggleCollapse}
+                className="px-1 py-[1px] text-xs text-white hover:text-black bg-neutral-800 hover:bg-white rounded-full cursor-pointer transition-all duration-300"
+              >
+                collapse
+              </button>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
 
-      <div className="absolute top-[105px] w-full h-full">
-        <div className="relative w-full h-full">
+      <div className="relative gap-1 flex flex-col items-center justify-center w-full">
+        <AnimatePresence>
           {cards.map((_, i) => (
             <NotificationCard
               key={i}
@@ -40,7 +42,7 @@ export default function NotificationsIOS() {
               onClick={toggleCollapse}
             />
           ))}
-        </div>
+        </AnimatePresence>
       </div>
     </div>
   );
@@ -57,24 +59,26 @@ function NotificationCard({
 }) {
   const variants = {
     expanded: {
-      y: index * 50, // espacement vertical (48px = h-12 + gap)
+      y: index * 1, // Espacement vertical entre les cartes
       scale: 1,
-      opacity: 1,
       zIndex: 10 - index,
+      opacity: 1,
       transition: {
-        duration: 0.4,
-        ease: "easeOut",
+        type: "spring",
+        stiffness: 100,
+        damping: 15,
         delay: index * 0.05,
       },
     },
     collapsed: {
-      y: -index * 5,
+      y: -index * 7,
       scale: 1 - index * 0.07,
-      opacity: 1 - index * 0.15,
       zIndex: 10 - index,
+      opacity: 1 - index * 0.15,
       transition: {
-        duration: 0.4,
-        ease: "linear",
+        type: "spring",
+        stiffness: 100,
+        damping: 15,
         delay: index * 0.05,
       },
     },
@@ -82,12 +86,14 @@ function NotificationCard({
 
   return (
     <motion.div
+      layout
       variants={variants}
       animate={isCollapse ? "collapsed" : "expanded"}
       initial={false}
       onClick={onClick}
       className={clsx(
-        "absolute left-0 right-0 mx-auto w-52 h-12 rounded-2xl bg-white shadow-md cursor-pointer"
+        "w-52 h-12 rounded-2xl bg-white shadow-md cursor-pointer",
+        isCollapse ? "absolute left-0 top-0 right-0 mx-auto" : "relative"
       )}
     />
   );
